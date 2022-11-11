06945c1dc4501d1234fe77d3f799d89414e0f6c8
AndrewF <andrew@sytrue.com>
2022-11-11 10:51:59 -0500

    Remove nat gateway

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,10 @@ output "subnet_internal" {
   value = "${aws_subnet.subnet_internal.id}"
 }
 
-output "nat_gateway_id" {
-  value = "${aws_nat_gateway.aws_nat_gateway.id}"
-}
-
-output "nat_gateway_eni" {
-  value = "${aws_nat_gateway.aws_nat_gateway.network_interface_id}"
-}
+#output "nat_gateway_id" {
+#  value = "${aws_nat_gateway.aws_nat_gateway.id}"
+#}
+#
+#output "nat_gateway_eni" {
+#  value = "${aws_nat_gateway.aws_nat_gateway.network_interface_id}"
+#}
